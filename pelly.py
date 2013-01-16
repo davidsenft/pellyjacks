52348c1cf3c3a3140cf569f3c52cce1c749ac65d
David Senft <dave@thisoneisme.com>
2013-01-15 23:54:48 -0500

    sqlite saving

--- pelly.py
+++ pelly.py
@@ -1,98 +1,215 @@
+###########################################
+
+import math
+import sqlite3
+import yaml
+
+###########################################
+
+# SQL Connection
+connection = sqlite3.connect('example.db')
+c = connection.cursor()
+
+# Create table
+# c.execute("CREATE TABLE boards (board text, score real)")
+
+
+###########################################
+
 def error(str):
 	print "//\n//\tERROR\n//\t" + str + "\n//"
+
+def msg(str):
+	print "> " + str + "..."
 
 def process(command):
 	print "You said: " + command
 
+def spot(r,c):
+	return "(" + `r` + ", " + `c` + ")"
+
+def debug(var, name):
+	if hasattr(var,"__dict__"):
+		print name + ": " + vars(var)
+	if type(var) == int:
+		print name + ": " + `var`
+	if type(var) == str:
+		print name + ": " + var
+
+###########################################
+
 class PellyBoard:
 
-	"Game board"
-	def __init__(self):
-		print "initializing board"
-		self.b = [[1,1,1,1],[1,1,1,1],[1,1,1,1],[1,1,1,1]]
-		self.t = 1
-		self.s1 = 0
-		self.s2 = 0
-		self.m = 0
+	# b (optional): List with initial board position
+	def __init__(self, b=[[1,1,1,1],[1,1,1,1],[1,1,1,1],[1,1,1,1]]):
+		msg("initializing PellyBoard")
+		self.b = b
+		self.rows = len(self.b)
+		self.cols = len(self.b[0])
+
+	def __eq__(self, other):
+		if type(other) is type(self):
+			return self.__dict__ == other.__dict__
+		return False
+
+	def __ne__(self, other):
+		return not self.__eq__(other)
 
 	def echo(self):
-		print "//"
-		print "// \tPELLYJACKS"
-		print "// \t" + `self.m` + " moves have been made" 
-		print "// \tplayer 1 score: " + `self.s1`
-		print "// \tplayer 2 score: " + `self.s2`
-		print "// \tplayer " + `self.t` + "'s move \n// "
 		for row in self.b:
 			print "// \t" + "\t".join([`col` for col in row])
 		print "// "
 
 	def html(self):
-		buf = "<table>"
+		html = "<table>"
 		for row in self.b:
-			buf += "<tr><td>"
-			buf +=  "</td><td>".join([`col` for col in row])
-			buf += "</td></tr>"
-		buf += "</table>"
-		print buf
+			html += "<tr><td>"
+			html +=  "</td><td>".join([`col` for col in row])
+			html += "</td></tr>"
+		html += "</table>"
+		print html
 
-	def move(self, fr, fc, tr, tc):
-		t = self.b[tr][tc]
+	def get(self, r, c):
+		return self.b[r][c]
 
-		if t > 0:
-			"stacky town"
-			return stack(fr,fc,tr,tc)
-
-		elif t == 0:
-			"slidey town"
-			return slide(fr,fc,tr,tc)
-
-		"funky town"
-		error("inconceivable!")
-		return False
-
-
-	def slide(self, fr, fc, tr, tc):
-		f = self.b[fr][fc]
-		t = self.b[tr][tc]
-
-		if f == 0:
-			error("no stack to slide")
+	def on(self, r, c):
+		if r < 0 or r >= len(self.b):
 			return False
-			
-		elif f == 1:
-			error("can't slide a jack")
+		if c < 0 or c >= len(self.b[0]):
 			return False
-
-		return self.domove(fr, fc, tr, tc)
-
-
-	def stack(self, fr, fc, tr, tc):
-		f = self.b[fr][fc]
-		t = self.b[tr][tc]
-
-		if f == 0:
-			error("no jack to stack")
-			return False
-
-		elif f > 1:
-			error("can't stack a stack")
-			return False
-
-		return self.domove(fr, fc, tr, tc)
-
-
-	def domove(self, fr, fc, tr, tc):
-		self.b[tr][tc] += self.b[fr][fc]
-		self.b[fr][fc] = 0
-		self.echo()
 		return True
 
+	def move(self, fr, fc, tr, tc):
+		self.b[tr][tc] += self.b[fr][fc]
+		self.b[fr][fc] = 0
 
-board = PellyBoard()
-board.stack(0,0,1,1)
 
-command = raw_input("Player " + `board.t` + "'s turn... \n")
-while (command != "stop"):
+
+###########################################
+
+class PellyGame:
+
+	# board (optional): PellyBoard with initial position
+	def __init__(self, board=PellyBoard(), player=1, rack1=0, rack2=0, moves=0):
+		msg("initializing PellyGame")
+		self.board = board
+		self.player = player
+		self.rack1 = rack1
+		self.rack2 = rack2
+		self.moves = moves
+
+	def __eq__(self, other):
+		if type(other) is type(self):
+			return self.__dict__ == other.__dict__
+		return False
+
+	def __ne__(self, other):
+		return not self.__eq__(other)
+
+	def echo(self):
+		print "//"
+		print "// \tPELLYJACKS " + `self.board.rows` + "x" + `self.board.cols`
+		print "// \tmoves made: " + `self.moves`
+		print "// \tmoves available: " + `self.movesleft()`
+		print "// \tplayer 1 rack: " + `self.rack1`
+		print "// \tplayer 2 rack: " + `self.rack2`
+		print "// \tplayer " + `self.player` + "'s move \n// "
+		self.board.echo()
+
+	def save(self):
+		msg("saving game position")
+		boardstring = yaml.dump(self.board.b)
+		c.execute("INSERT INTO boards VALUES ('" + boardstring + "', " + `self.moves` + ")")
+		# Save (commit) the changes
+		connection.commit()
+
+	def movesleft(self):
+
+		num = 0
+		#for every spot on the grid...
+		for r in range(self.board.rows):
+			for c in range(self.board.cols):
+
+				# try all four directions
+				for change in [[0,-1],[-1,0],[0,1],[1,0]]:
+					if self.move(r,c,r+change[0],c+change[1],False):
+						num = num + 1
+
+		return num
+
+	def move(self, fr, fc, tr, tc, do=True):
+
+		if do:
+			msg("checking move " + spot(fr,fc) + " to " + spot(tr,tc))
+
+		if not self.board.on(fr,fc) or not self.board.on(tr,tc):
+			if do:
+				error("out of bounds")
+			return False
+
+		if not ((tr - fr)**2 + (tc - fc)**2) == 1:
+			if do:
+				error("spaces are not adjacent")
+			return False
+
+		f = self.board.get(fr,fc)
+		t = self.board.get(tr,tc)
+
+		if f <= 0:
+			if do:
+				error("no piece to move")
+			return False
+
+		if t > 0:
+			if f > 1:
+				if do:
+					error("can't stack a stack")
+				return False
+			action = "stacking"
+
+		elif t == 0:
+			if f == 1:
+				if do:
+					error("can't slide a jack")
+				return False
+			action = "sliding"
+
+		else:
+			error("inconceivable!")
+			return False
+		
+		if do:
+			msg("move is valid")
+
+		if do:
+			msg(action + " piece at " + spot(fr,fc) + " to " + spot(tr,tc))
+			self.board.move(fr, fc, tr, tc)
+			self.player = -self.player
+			self.moves = self.moves + 1
+			self.echo()
+
+		return True
+
+	def rack(self, col, do=True):
+		# do some stuff
+		print ""
+
+###########################################
+
+game = PellyGame()
+game.move(0,0,0,0)
+game.move(0,1,0,2)
+game.move(0,0,1,0)
+game.move(3,3,2,3)
+
+# print "YAML DUMP: " + yaml.dump(board)
+
+msg("player " + `game.player` + "'s turn")
+command = raw_input()
+while (command != "q"):
 	process(command)
-	command = raw_input("Player " + `board.t` + "'s turn... \n")
+	msg("player " + `game.player` + "'s turn")
+	command = raw_input()
 
+# Close the database connection
+connection.close()
